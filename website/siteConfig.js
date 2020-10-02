b7ff26c26260d3cdcf6fe71145bfd64f38cffe22
SpandanaNune <spandana.nune@gmail.com>
2020-10-02 12:25:26 -0700

    Updated with my details for portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Spandana Nune', // Title for your website.
+    tagline: 'Software Engineer, love to learn new things',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://github.com/SpandanaNune', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'SpandanaNune',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'spandana-nune-a82067ab',
         twitter: '',
-        github: '',
+        github: 'SpandanaNune',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
